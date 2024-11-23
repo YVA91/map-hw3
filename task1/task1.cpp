10cbaeac042ea8b9b9104097b1c66b1008307b93
Vladimir Yakhnev <Yahnev91@yandex.ru>
2024-11-23 12:52:50 +0300

    Задание 3

---
+++ task1/task1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <thread>
+#include <vector>
+#include <future>
+#include <chrono>
+
+int find_min(std::vector<int> v, int item)
+{
+	size_t size = v.size();
+	int min = item;
+
+	for (int i = item; i < size; i++) {
+		if (v[i] < v[min])
+		{
+			min = i;
+		}
+	}
+	return min;
+}
+
+void sort(std::vector<int>& v, std::promise<std::vector<int>> prom) {
+
+	int min = 0;
+	int item = 0;
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		auto f = std::async(std::launch::async, find_min, v, item);
+		min = f.get();
+
+		item++;
+		int p = v[min];
+		v[min] = v[i];
+		v[i] = p;
+	}
+}
+
+
+
+int main()
+{
+	std::vector<int> v{ 8, 2, 4, 6, 9, 3, 1, 5, 7 };
+
+	auto start = std::chrono::steady_clock::now();
+
+	std::promise<std::vector<int>> prom;
+	std::future<std::vector<int>> ft_res = prom.get_future();
+	sort(std::ref(v), std::move(prom));
+
+	auto end = std::chrono::steady_clock::now();
+	auto diff = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+	std::cout << "Time slow: " << diff.count() << std::endl;
+
+	for (const auto& i : v)
+	{
+		std::cout << i << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
---
+++ task2/task2.cpp
@@ -0,0 +1,40 @@
+﻿#include <algorithm>
+#include <chrono>
+#include <exception>
+#include <future>
+#include <iostream>
+#include <numeric>
+#include <thread>
+#include <vector>
+#include <random>
+
+template<typename Iterator, typename T>
+T para_forEach(Iterator first, Iterator last, T init)
+{
+	int length = std::distance(first, last);
+	int max_chunk_size = std::thread::hardware_concurrency();
+	if (length <= max_chunk_size)
+	{
+		return std::for_each(first, last, init);
+	}
+	else
+	{
+		Iterator mid_point = first;
+		std::advance(mid_point, length / 2);
+		std::future<T> first_half_result = std::async(para_forEach<Iterator, T>, first, mid_point, init);
+
+		return std::for_each(mid_point, last, init);
+	}
+}
+
+void fn(int x) {
+	std::cout << x * 2 << std::endl;
+}
+
+int main()
+{
+	std::vector<int> V{ 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24 };
+	auto  res = para_forEach(V.begin(), V.end(), fn);
+
+	return 0;
+}
